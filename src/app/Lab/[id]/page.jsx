99a9fe85d9f974549b51da3e67d3fd0df857f542
SanyaTangook <93518732+SanyaTangook@users.noreply.github.com>
2024-05-20 23:16:18 +0700

    Edit Page Lab

--- src/app/Lab/[id]/page.jsx
+++ src/app/Lab/[id]/page.jsx
@@ -1,6 +1,15 @@
+"use client";
+import axios from "axios";
 import Link from "next/link";
 
 export default function lab() {
+  const onClickLab = () => {
+    axios
+      .post("http://localhost:3001/containers/ubuntu:latest")
+      .then(console.log("ผ่าน"));
+  };
+
+  
   return (
     <>
       <header>
@@ -13,11 +22,27 @@ export default function lab() {
               <Link href="/Room">Room</Link>
             </li>
             <li>
-              <Link href='#'>Lab</Link>
+              <Link href="#">Lab</Link>
             </li>
           </ul>
         </div>
       </header>
+      <section>
+        <nav>
+          <li>
+            <button className="btn btn-square" onClick={onClickLab}>
+              {" "}
+              เริ่มทำ{" "}
+            </button>
+          </li>
+          <li>
+            <button className="btn" onClick={onClickLab}>
+              {" "}
+              ตรวจคำตอบ{" "}
+            </button>
+          </li>
+        </nav>
+      </section>
     </>
   );
 }
